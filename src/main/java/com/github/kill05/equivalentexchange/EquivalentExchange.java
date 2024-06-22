a2cbacdc486c7c1b9b92eab9a9054c326ed8c7b0
kill05 <vinkimeme@libero.it>
2024-06-22 02:10:01 +0200

    Made main class final

--- src/main/java/com/github/kill05/equivalentexchange/EquivalentExchange.java
+++ src/main/java/com/github/kill05/equivalentexchange/EquivalentExchange.java
@@ -23,7 +23,7 @@ import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class EquivalentExchange implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public final class EquivalentExchange implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
 
     public static final String MOD_ID = "equivalentexchange";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
